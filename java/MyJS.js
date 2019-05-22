d28322bb3f20c8d32a602403baf62768f8d9a5f5
Donatas <donatasrackausk@gmail.com>
2019-05-22 12:29:51 +0200

    heyho

--- java/MyJS.js
+++ java/MyJS.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
     $('#burger').click(function () {
         $('.dropdown').toggleClass('clicked');
-        $('.body').toggleClass('clicked');
+        $('.body').fadeToggle(500);
+        $('.header').fadeToggle(500);
         $('.burger-box').toggleClass('clicked');
-		$('body').toggleClass('clicked');
-		$('html').toggleClass('clicked');
-    })
-	
-	
-	
+    });
+
 });
