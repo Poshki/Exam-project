945f3db88708792a3a25bfb7d0d1db6ca50633b2
Donatas <donatasrackausk@gmail.com>
2019-05-26 14:30:39 +0200

    hi

--- java/MyJS-product.js
+++ java/MyJS-product.js
@@ -20,6 +20,47 @@ $(document).ready(function () {
 		$('#popper').toggleClass('active');
 		$('#pop-up').toggleClass('active');
 	});
+	
+	
+	
+	
+	$('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+    }
+  ]
+    });
 });
 
 
