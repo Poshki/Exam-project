5be6c45d4a9fde4426d72c907558cfc582508aa7
Donatas <donatasrackausk@gmail.com>
2019-05-26 19:24:41 +0200

    hi

--- java/MyJS-product.js
+++ java/MyJS-product.js
@@ -5,26 +5,26 @@ $(document).ready(function () {
         $('.header').fadeToggle(500);
         $('.burger-box').toggleClass('clicked');
     });
-	
-	$('.add-to-cart').click(function(){
-		$('.pop-up').toggleClass('active');
-		$('.pop-up-text').toggleClass('active');
-	});
-	
-	$('#popper').click(function(){
-		$('#popper').toggleClass('active');
-		$('#pop-up').toggleClass('active');
-	});
-	
-	$('.close').click(function(){
-		$('#popper').toggleClass('active');
-		$('#pop-up').toggleClass('active');
-	});
-	
-	
-	
-	
-	$('.carousel').slick({
+
+    $('.add-to-cart').click(function () {
+        $('.pop-up').toggleClass('active');
+        $('.pop-up-text').toggleClass('active');
+    });
+
+    $('#popper').click(function () {
+        $('#popper').toggleClass('active');
+        $('#pop-up').toggleClass('active');
+    });
+
+    $('.close').click(function () {
+        $('#popper').toggleClass('active');
+        $('#pop-up').toggleClass('active');
+    });
+
+
+
+
+    $('.carousel').slick({
         dots: true,
         infinite: true,
         speed: 300,
@@ -66,16 +66,16 @@ $(document).ready(function () {
 
 
 function increaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number').value = value;
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
 }
 
 function decreaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number').value = value;
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
 }
