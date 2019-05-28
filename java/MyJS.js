e91a5f0fcecd63cfb536d0401dd2a84e2648a289
Donatas <donatasrackausk@gmail.com>
2019-05-28 10:56:57 +0200

    exam

--- java/MyJS.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#burger').click(function () {
-        $('.dropdown').toggleClass('clicked');
-        $('.body').fadeToggle(500);
-        $('.header').fadeToggle(500);
-        $('.burger-box').toggleClass('clicked');
-    });
-
-});
